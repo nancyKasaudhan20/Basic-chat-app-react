ca390696c464adfe43a3c11994c2cb792a2bafad
Nancy <nancy20@gmail.com>
2024-01-26 04:03:22 +0530

    UI completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,39 @@
-
+import {Box, Button, Container, Input, VStack, HStack} from '@chakra-ui/react'
+import Message from './Components/Message';
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+   <Box bg={"purple.100"}>
+    <Container bg={"white"} h={"100vh"}> 
+     <VStack /*bg={"telegram.100"}*/ h={"full"} paddingY={"2"}>
+       <Button w={"full"} colorScheme={"pink"}> Logout</Button>
+
+     <VStack bg={"red.100"} h={"full"} w={'full'} overflowY="auto">
+  {/* MESSAGEEEEE */}
+         <Message text={"Sample message"}/>
+         <Message user="me" text={"Sample message"}/>
+         <Message user="me" text={"Sample message"}/>
+         <Message user="me" text={"Sample message"}/>
+         <Message text={"Sample message"}/>
+         <Message user="me" text={"Sample message"}/>
+         <Message text={"Sample message"}/>
+         <Message user="me" text={"Sample message"}/>
+         <Message text={"Sample message"}/>
+         <Message text={"Sample message"}/>
+         <Message text={"Sample message"}/>
+         <Message text={"Sample message"}/>
+         <Message text={"Sample message"}/>
+         <Message text={"Sample message"}/>
+     </VStack> 
+
+   <form style={{ width:"100%"}}action="">
+      <HStack> 
+        <Input placeholder='Enter the Message' />
+        <Button colorScheme={"purple"} type='submit'>Send</Button>
+      </HStack>
+  </form>
+     </VStack>
+    </Container>
+   </Box>
   );
 }
 
